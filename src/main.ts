2c16cc6a6c06c62d3c1d468f22a3470ece04aa60
deepakpaliwal <deepak.paliwal@redbus.com>
2023-06-05 18:41:18 +0530

    cookie-parser bug fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const { PORT } = process.env;
